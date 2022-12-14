8b1b1d369150c7102b4d3980ecee3ffac3015a4a
Bakary Sylla <syllabakary2002@gmail.com>
2022-12-14 09:42:17 -0800

    update different professors with @Autowired

--- springAnnotation/src/springAnnotation/application/EnglishProfessor.java
+++ springAnnotation/src/springAnnotation/application/EnglishProfessor.java
@@ -1,11 +1,18 @@
 package springAnnotation.application;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EnglishProfessor implements Professor {
 	
 	private SchoolAdmin schoolAdmin;
+	
+	@Autowired
+	//added constructor method
+	public EnglishProfessor(SchoolAdmin theSchoolAdmin) {
+		this.schoolAdmin = theSchoolAdmin;
+	}
 
 	@Override
 	public String getDailyNote() {
--- springAnnotation/src/springAnnotation/application/MathProfessor.java
+++ springAnnotation/src/springAnnotation/application/MathProfessor.java
@@ -1,11 +1,19 @@
 package springAnnotation.application;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class MathProfessor implements Professor {
 	
 	private SchoolAdmin schoolAdmin;
+	
+	//added Constructor method
+	@Autowired
+	public MathProfessor(SchoolAdmin theSchoolAdmin) {
+		this.schoolAdmin = theSchoolAdmin;
+	}
 
 	@Override
 	public String getDailyNote() {
